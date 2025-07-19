1a317894b3cf18755ca3a8731508e6d639942e5e
Gsravanthi1 <gajulapalli.sravanthi1@gmail.com>
2025-07-19 20:32:13 +0530

    Create LoanServiceApplication.java

---
+++ Microservices with Spring Boot 3 and Spring Cloud /Microservices with API gateway/Create Eureka Discovery Server and register/3. Loan Service/main/java/LoanServiceApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LoanServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoanServiceApplication.class, args);
+    }
+}
