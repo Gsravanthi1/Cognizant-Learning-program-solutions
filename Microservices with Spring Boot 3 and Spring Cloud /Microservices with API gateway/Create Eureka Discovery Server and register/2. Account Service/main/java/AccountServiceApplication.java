bc9d586a0bbf4b71b9e9480881503313f41d92f0
Gsravanthi1 <gajulapalli.sravanthi1@gmail.com>
2025-07-19 20:26:55 +0530

    Create AccountServiceApplication.java

---
+++ Microservices with Spring Boot 3 and Spring Cloud /Microservices with API gateway/Create Eureka Discovery Server and register/2. Account Service/main/java/AccountServiceApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AccountServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
+}
