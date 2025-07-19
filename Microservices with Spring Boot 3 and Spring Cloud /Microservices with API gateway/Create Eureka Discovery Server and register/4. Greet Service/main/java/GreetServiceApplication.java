0cb95a278874f54d75d27c5e923cf260b9bfcbd9
Gsravanthi1 <gajulapalli.sravanthi1@gmail.com>
2025-07-19 20:36:08 +0530

    Create GreetServiceApplication.java

---
+++ Microservices with Spring Boot 3 and Spring Cloud /Microservices with API gateway/Create Eureka Discovery Server and register/4. Greet Service/main/java/GreetServiceApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GreetServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreetServiceApplication.class, args);
+    }
+}
